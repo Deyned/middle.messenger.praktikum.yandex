ddc4a431728697b5ce879adc4412bd2e5eed2ba8
Andrey Pavlov <a.pavlov@sigma-it.ru>
2022-07-23 13:51:25 +0500

    netlify

---
+++ server.js
@@ -0,0 +1,15 @@
+//eslint-disable-next-line @typescript-eslint/no-var-requires
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static(__dirname + '/dist'));
+
+app.get('*', (req, res) => {
+	res.sendFile(__dirname + '/dist/index.html');
+});
+
+app.listen(PORT, function () {
+	console.log(`App listening on port ${PORT}, __dirname=${__dirname}!`);
+});
